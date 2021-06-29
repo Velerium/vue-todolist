025b598e603743985d35460d3e8653570eb08d61
Velerium <bondaniandrea@libero.it>
2021-06-29 16:42:42 +0200

    Decorated a bit. Thinking of something else...

--- js/script.js
+++ js/script.js
@@ -5,10 +5,11 @@ new Vue (
         el: '#app',
         data: {
             todo: [
-                'thing',
-                'thing',
-                'thing',
-                'things'
+                'Comprare il latte',
+                'Fare i compiti',
+                'Ricevere log-in bonus',
+                'Fare giro in bici',
+                'DORMIRE!!!'
             ],
 
             addToList: '',
@@ -16,16 +17,21 @@ new Vue (
 
         methods: {
             add: function() {
+
                 if(this.addToList === '') {
                     return;
                 }
                 this.todo.push(this.addToList);
+                document.getElementsByClassName('todo')[0].style.display = 'block';
                 this.addToList = '';
                 document.getElementById('more').focus();
             },
 
             remove: function(index) {
                 this.todo.splice(index, 1);
+                if (this.todo.length === 0) {
+                    document.getElementsByClassName('todo')[0].style.display = 'none';
+                }
             }
         },
     }
